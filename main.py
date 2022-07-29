11e65e254dfe0279de0f1232798cab024f33bf9f
avion <Avionk.smith@gmail.com>
2022-07-29 13:50:06 -0400

    Added conditional ststments and conversion formula.

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
-#this application converts farenheit to celsius
+#set my user input to represent the temperature.
 
-def convert(c):
-    F = (9/5)*c + 32
-    return (F)
+temperature = (input("Add a number for temeratue (e.g. 45F or 30C.) "))
+degree = int(temperature[:-1])
+intial_conversion = temperature[:-1]
 
-cel = float(input("Enter the temperature: "))
-Fah = convert(cel)
-print ("The temperature in Fahrenheit is ", Fah)
+if intial_conversion.upper == "C":
+    result = int(round((9 * degree) / 5 + 32))
+    o_conversion = "fahrenheit"
+
+elif intial_conversion.upper == "F":
+    result = int(round((degree - 32) * 5 / 9))
+    o_conversion = "celcius"
+
+else:
+    print("input proper conversion")
+    quit()
+
+print("The temperature in", o_conversion, "is", result, "degrees.")
+
+
+
